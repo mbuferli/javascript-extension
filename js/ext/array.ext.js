bdf012e9403112193aa6d02c95a4a660797e9cf0
Matteo Buferli <matteo.buferli@gmail.com>
2013-07-08 15:14:43 +0200

    delete duplicated files 2

--- js/ext/array.ext.js
+++ js/ext/array.ext.js
@@ -1,15 +1,15 @@
-Array.prototype.sortAscByProp = function(p){
- return this.sort(function(a,b){
-  return (a[p] > b[p]) ? 1 : (a[p] < b[p]) ? -1 : 0;
- });
+Array.prototype.sortAscByProp = function( p ){
+  return this.sort(function(a,b){
+    return (a[p] > b[p]) ? 1 : (a[p] < b[p]) ? -1 : 0;
+  });
 };
-Array.prototype.sortDescByProp = function(p){
- return this.sort(function(a,b){
-  return (a[p] < b[p]) ? 1 : (a[p] > b[p]) ? -1 : 0;
- });
+Array.prototype.sortDescByProp = function( p ){
+  return this.sort(function(a,b){
+    return (a[p] < b[p]) ? 1 : (a[p] > b[p]) ? -1 : 0;
+  });
 };
-Array.prototype.pushUnique = function (obj, equalityFunction) {
-  equalityFunction  = (typeof equalityFunction === "undefined") ? function(a, b) { return a === b } : equalityFunction;
+Array.prototype.pushUnique = function ( obj, equalityFunction ) {
+  equalityFunction  = (typeof equalityFunction === "undefined") ? function(a, b) { return a === b; } : equalityFunction;
   for(var n = 0; n < this.length; n++) {
     if(equalityFunction(this[n], obj)) { // contains
       return false;
@@ -17,4 +17,9 @@ Array.prototype.pushUnique = function (obj, equalityFunction) {
   }
   this.push(obj);
   return true;
-}
+};
+Array.prototype.remove = function( from, to ) {
+  var rest = this.slice((to || from) + 1 || this.length);
+  this.length = from < 0 ? this.length + from : from;
+  return this.push.apply(this, rest);
+};
