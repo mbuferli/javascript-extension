0fb584adf84adff25173525d3fce09dc72b10966
Matteo Buferli <matteo.buferli@gmail.com>
2013-08-28 11:15:48 +0200

    new unique function

--- js/ext/array.ext.js
+++ js/ext/array.ext.js
@@ -23,3 +23,9 @@ Array.prototype.remove = function( from, to ) {
   this.length = from < 0 ? this.length + from : from;
   return this.push.apply(this, rest);
 };
+Array.prototype.unique = function() {    
+	var o = {}, i, l = this.length, r = [];    
+	for(i=0; i<l;i+=1) o[this[i]] = this[i];    
+	for(i in o) r.push(o[i]);    
+	return r;
+};
